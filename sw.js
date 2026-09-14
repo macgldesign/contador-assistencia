21a2629f16f87240ae13d486f21709837ea4d3ae
macgldesign <110030689+macgldesign@users.noreply.github.com>
2026-09-14 10:12:59 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'contador-assistencia-v1';
+const ASSETS = [
+  '/contador-assistencia/',
+  '/contador-assistencia/index.html',
+  '/contador-assistencia/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
