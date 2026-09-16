f18c1606c1a4ce40ae40cf4f32057a8a3e2e5311
macgldesign <110030689+macgldesign@users.noreply.github.com>
2026-09-15 22:38:23 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'contador-assistencia-v1.0.41';
+const CACHE_NAME = 'contador-assistencia-v1.0.42';
 const ASSETS = [
   '/contador-assistencia/',
   '/contador-assistencia/index.html',
